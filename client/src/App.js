f5ae7158d8b7126c2b614b35e68c3549fce36dc3
Fernannd023 <fleamerksual@gmail.com>
2025-08-25 15:24:32 -0500

    installed bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import User from './components/getuser/User';
+import AddUser from './components/adduser/AddUser';
+import UpdateUser from './components/updateuser/UpdateUser';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <User />
+    },
+    {
+      path: "/add",
+      element: <AddUser />
+    },
+    {
+      path: "/update/:id",
+      element: <UpdateUser />
+    }
+  ])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RouterProvider router={route}></RouterProvider>
+      </header>
+    </div>
+  );
+}
+
+export default App;
